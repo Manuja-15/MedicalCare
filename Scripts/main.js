2629e1d44996d68b1c93322b51d31469f3c2a78c
Manuja-15 <manujaprathapa@gmail.com>
2024-12-13 19:36:49 +0530

    final updated js

--- Scripts/main.js
+++ Scripts/main.js
@@ -97,9 +97,34 @@ if ('serviceWorker' in navigator) {
         window.location.href = "checkout.html";
     });
 
-    // Submit form functionality
-    function submitForm() {
-        const form = document.getElementById("checkout-form");
+   
+    
+    // function submitForm(event) {
+    //     event.preventDefault(); // Prevent the form from reloading the page
+    
+    //     const form = document.getElementById("checkout-form");
+    
+    //     const firstName = document.getElementById("first-name").value;
+    //     const lastName = document.getElementById("last-name").value;
+    //     const address = document.getElementById("address").value;
+    
+    //     const deliveryDate = new Date();
+    //     deliveryDate.setDate(deliveryDate.getDate() + 3);
+    
+    //     alert(
+    //         `Thank you for your purchase, ${firstName} ${lastName}!\n\nYour order will be delivered to:\n${address}\n\nExpected delivery date: ${deliveryDate.toDateString()}`
+    //     );
+    
+    //     form.reset(); // Reset the form fields
+    // }
+    
+});
+
+ // Submit form functionality
+ document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("checkout-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent page reload
 
         const firstName = document.getElementById("first-name").value;
         const lastName = document.getElementById("last-name").value;
@@ -111,10 +136,10 @@ if ('serviceWorker' in navigator) {
         alert(
             `Thank you for your purchase, ${firstName} ${lastName}!\n\nYour order will be delivered to:\n${address}\n\nExpected delivery date: ${deliveryDate.toDateString()}`
         );
-
-        form.reset(); // Reset the form fields
-    }
+        
+        const form = document.getElementById("checkout-form");
+        form.reset();
+    });
 });
-
 
 
